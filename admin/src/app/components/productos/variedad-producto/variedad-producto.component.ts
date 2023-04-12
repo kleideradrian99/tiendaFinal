9e271b9f9c2b3ba210fa6e9fcf172d62b2425705
Adrian <kleiidermachado@gmail.com>
2023-04-12 17:33:15 -0500

    Sin Cambios

--- admin/src/app/components/productos/variedad-producto/variedad-producto.component.ts
+++ admin/src/app/components/productos/variedad-producto/variedad-producto.component.ts
@@ -11,7 +11,7 @@ declare var iziToast;
 })
 export class VariedadProductoComponent implements OnInit {
 
-  public producto :any = {};
+  public producto: any = {};
   public id;
   public token;
 
@@ -20,33 +20,33 @@ export class VariedadProductoComponent implements OnInit {
   public url;
 
   constructor(
-    private _route:ActivatedRoute,
-    private _productoService : ProductoService
-  ) { 
+    private _route: ActivatedRoute,
+    private _productoService: ProductoService
+  ) {
     this.token = localStorage.getItem('token');
     this.url = GLOBAL.url;
     this._route.params.subscribe(
-      params=>{
+      params => {
         this.id = params['id'];
 
 
-        this._productoService.obtener_producto_admin(this.id,this.token).subscribe(
-          response=>{
-           if(response.data == undefined){
-            this.producto = undefined;
-           }else{
-             this.producto = response.data;
+        this._productoService.obtener_producto_admin(this.id, this.token).subscribe(
+          response => {
+            if (response.data == undefined) {
+              this.producto = undefined;
+            } else {
+              this.producto = response.data;
 
-           }
-           console.log(this.producto);
-           
-            
+            }
+            console.log(this.producto);
+
+
           },
-          error=>{
+          error => {
 
           }
         );
-        
+
       }
     );
   }
@@ -55,69 +55,68 @@ export class VariedadProductoComponent implements OnInit {
   }
 
 
-  agregar_variedad(){
-    if(this.nueva_variedad){
-
-      this.producto.variedades.push({titulo: this.nueva_variedad});
+  agregar_variedad() {
+    if (this.nueva_variedad) {
+      this.producto.variedades.push({ titulo: this.nueva_variedad });
       this.nueva_variedad = '';
-    }else{
+    } else {
       iziToast.show({
-          title: 'ERROR',
-          titleColor: '#FF0000',
-          color: '#FFF',
-          class: 'text-danger',
-          position: 'topRight',
-          message: 'El campo de la variedad debe ser completada'
+        title: 'ERROR',
+        titleColor: '#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'El campo de la variedad debe ser completada'
       });
-   
+
     }
   }
 
-  eliminar_variedad(idx){
-    this.producto.variedades.splice(idx,1);
+  eliminar_variedad(idx) {
+    this.producto.variedades.splice(idx, 1);
   }
 
-  actualizar(){
-    if(this.producto.titulo_variedad){
-      if(this.producto.variedades.length >= 1){
-          //actualizar
-          this.load_btn = true;
+  actualizar() {
+    if (this.producto.titulo_variedad) {
+      if (this.producto.variedades.length >= 1) {
+        //actualizar
+        this.load_btn = true;
         this._productoService.actualizar_producto_variedades_admin({
           titulo_variedad: this.producto.titulo_variedad,
-          variedades : this.producto.variedades
-        },this.id,this.token).subscribe(
-          response=>{
+          variedades: this.producto.variedades
+        }, this.id, this.token).subscribe(
+          response => {
             console.log(response);
 
             iziToast.show({
-                title: 'SUCCESS',
-                titleColor: '#1DC74C',
-                color: '#FFF',
-                class: 'text-success',
-                position: 'topRight',
-                message: 'Se actualizó correctamente lass variedades.'
+              title: 'SUCCESS',
+              titleColor: '#1DC74C',
+              color: '#FFF',
+              class: 'text-success',
+              position: 'topRight',
+              message: 'Se actualizó correctamente lass variedades.'
             });
             this.load_btn = false;
           }
         );
-      }else{
+      } else {
         iziToast.show({
-            title: 'ERROR',
-            titleColor: '#FF0000',
-            color: '#FFF',
-            class: 'text-danger',
-            position: 'topRight',
-            message: 'Se debe agregar al menos una variedad'
-        });
-      }
-    }else{
-      iziToast.show({
           title: 'ERROR',
           titleColor: '#FF0000',
           color: '#FFF',
           class: 'text-danger',
           position: 'topRight',
-          message: 'Debe completar el titulo de la variedad'
+          message: 'Se debe agregar al menos una variedad'
+        });
+      }
+    } else {
+      iziToast.show({
+        title: 'ERROR',
+        titleColor: '#FF0000',
+        color: '#FFF',
+        class: 'text-danger',
+        position: 'topRight',
+        message: 'Debe completar el titulo de la variedad'
       });
     }
   }
