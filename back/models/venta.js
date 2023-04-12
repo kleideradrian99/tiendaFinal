1e9396ee210642f1bfaca0ab03a87d47b9fd6f5a
Adrian <kleiidermachado@gmail.com>
2023-04-12 17:33:44 -0500

    Confirmas

--- back/models/venta.js
+++ back/models/venta.js
@@ -4,17 +4,18 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var VentaSchema = Schema({
-    cliente: {type: Schema.ObjectId, ref: 'cliente', required: true},
-    nventa: {type: String, require: true},
-    subtotal: {type: Number, require: true},
-    envio_titulo: {type: String, require: true},
-    envio_precio: {type: Number, require: true},
-    transaccion: {type: String, require: true},
-    cupon: {type: String, require: true},
-    estado: {type: String, require: true},
-    direccion: {type: Schema.ObjectId, ref: 'direccion', require: true},
-    nota: {type: String, require: true},
-    createdAt: {type:Date, default: Date.now, require: true}
+    cliente: { type: Schema.ObjectId, ref: 'cliente', required: true },
+    nventa: { type: String, require: true },
+    subtotal: { type: Number, require: true },
+    envio_titulo: { type: String, require: false },
+    envio_precio: { type: Number, require: false },
+    transaccion: { type: String, require: false },
+    cupon: { type: String, require: true },
+    estado: { type: String, require: true },
+    direccion: { type: Schema.ObjectId, ref: 'direccion', require: true },
+    nota: { type: String, require: true },
+    userid: { type: Schema.ObjectId, ref: 'admin', required: false },
+    createdAt: { type: Date, default: Date.now, require: true }
 });
 
-module.exports =  mongoose.model('venta',VentaSchema);
+module.exports = mongoose.model('venta', VentaSchema);
